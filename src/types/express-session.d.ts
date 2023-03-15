b5d2d8bc3879d7566a80aafa74bf7d1d23aeda7b
jackthechrest <120044450+jackthechrest@users.noreply.github.com>
2023-03-15 11:26:14 -0500

    Update express-session.d.ts

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -4,7 +4,13 @@ declare module 'express-session' {
   export interface Session {
     clearSession(): Promise<void>; // DO NOT MODIFY THIS!
 
-    // NOTES: Add your app's custom session properties here:
-   
+     // NOTES: Add your app's custom session properties here:
+     authenticatedUser: {
+        userId: string;
+        isPro: boolean;
+        isAdmin: boolean;
+        username: string;
+     }
+    loggedIn: boolean;
   }
 }
