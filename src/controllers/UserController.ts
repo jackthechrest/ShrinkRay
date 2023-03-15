6965da90872e69c944608d13d872f1ef3a11ad29
jackthechrest <120044450+jackthechrest@users.noreply.github.com>
2023-03-15 11:52:34 -0500

    Update UserController.ts

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,1 +1,9 @@
 
+async function registerUser(req: Request, res: Response): Promise<void> {
+    // TODO: Implement the registration code
+    // Make sure to check if the user with the given username exists before attempting to add the account
+    // Make sure to hash the password before adding it to the database
+    // Wrap the call to `addNewUser` in a try/catch like in the sample code
+}
+
+export {};
