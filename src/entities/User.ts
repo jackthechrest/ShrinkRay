1d54cf795931c413fd3ae3e2d6b9b5ad85111459
jackthechrest <120044450+jackthechrest@users.noreply.github.com>
2023-03-15 11:11:29 -0500

    Create User.ts

---
+++ src/entities/User.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  passwordHash: string;
+
+  @Column({ default: false })
+  isPro: boolean;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+}
