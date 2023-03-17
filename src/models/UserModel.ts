6e66a910e501eeeecb5e194697a4d0d68ceb4a10
jackthechrest <120044450+jackthechrest@users.noreply.github.com>
2023-03-17 09:50:12 -0500

    Update UserModel.ts

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -3,6 +3,17 @@ import { User } from '../entities/User';
 
 const userRepository = AppDataSource.getRepository(User);
 
+async function addNewUser(username: string, passwordHash: string): Promise<User>  {
+   // Create the new user object
+  let newUser = new User();
+  newUser.username = username;
+  newUser.passwordHash = passwordHash;
+  
+  newUser = await userRepository.save(newUser);
+
+  return newUser;
+}
+
 async function getUserById(userId: string): Promise<User | null> {
   const queriedUser = await userRepository.findOne({
     select: {
@@ -20,10 +31,17 @@ async function getUserById(userId: string): Promise<User | null> {
 async function getUserByUsername(username: string): Promise<User | null> {
     // TODO: Get the user by where the username matches the parameter
     // This should also retrieve the `links` relation
+    const queriedUser = await userRepository.findOne({
+    select: {
+      userId: true,
+      username: true,
+      isPro: true,
+      isAdmin: true,
+      links: true,
+    },
+    where: { username },
+    });
+    return queriedUser;
 }
 
-async function addNewUser(username: string, passwordHash: string): Promise<User | null> {
-    // TODO: Add the new user to the database
-}
-
-export {};
+export {addNewUser, getUserById, getUserByUsername};
